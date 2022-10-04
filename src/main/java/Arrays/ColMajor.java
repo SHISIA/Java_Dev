b277999fd15681c9b7ad63067a3af537c44c0a36
apple <jamesshisia6@gmail.com>
2022-10-04 13:02:17 +0300

    Practised with Java ColMajor 2D array

--- src/main/java/Arrays/ColMajor.java
+++ src/main/java/Arrays/ColMajor.java
@@ -1,13 +1,27 @@
 package Arrays;
 
 public class ColMajor {
-    static String[][] color={{"red","blue"},{"Green","yellow"},{"Pink","Purple"},{"ashley","zack"}};
+    static String[][] color={{"red","blue"},
+                            {"Green","yellow"},
+                            {"Pink","Purple"},
+                            {"ashley","zack"}};
     public static void main(String[] args) {
-        for (int i=0;i<color[0].length;i++){
-            for (int j=0;j<color.length;j++){
-                System.out.println(i);
-                System.out.println(j);
-                System.out.println("ColMajor "+color[j][i]);
+//        for (int i=0;i<color[0].length;i++){
+//            for (int j=0;j<color.length;j++){
+//                System.out.println(i);
+//                System.out.println(j);
+//                System.out.println("ColMajor "+color[j][i]);
+//            }
+//        }
+        practise();
+    }
+
+    public static void practise(){
+        int[][] numArray={{1,6},{2,7},{3,8},{4,9},{5,10}};
+        //column major to print values 1-10 in order
+        for (int i=0;i<numArray[0].length;i++){
+            for (int j=0;j<numArray.length;j++){
+                System.out.println("Number: "+numArray[j][i]);
             }
         }
     }
